b6bce1d3176687472d0a70043494005da7a71057
lingyun1010 <zhaolingyun1010@gmail.com>
2019-01-15 16:55:21 +0000

    Initialize component name and props

--- __test__/MyComponent.test.js
+++ __test__/MyComponent.test.js
@@ -4,29 +4,29 @@ import Enzyme from 'enzyme'
 import {shallow, mount, render} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-import MyComponent from '../src/MyComponent.js'
+import ExperimentTable from '../src/ExperimentTable.js'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe(`MyComponent`, () => {
   test(`should render without throwing an error`, () => {
-     expect(shallow(<MyComponent />).contains(<div className="foo">Bar</div>)).toBe(true)
+     expect(shallow(<ExperimentTable />).contains(<div className="foo">Bar</div>)).toBe(true)
   })
 
   test(`should be selectable by class "foo"`, () => {
-    expect(shallow(<MyComponent />).is(`.foo`)).toBe(true)
+    expect(shallow(<ExperimentTable />).is(`.foo`)).toBe(true)
   })
 
   test(`should mount in a full DOM`, () => {
-    expect(mount(<MyComponent />).find(`.foo`)).toHaveLength(1)
+    expect(mount(<ExperimentTable />).find(`.foo`)).toHaveLength(1)
   })
 
   test(`should render to static HTML`, () => {
-    expect(render(<MyComponent />).text()).toEqual(`Bar`)
+    expect(render(<ExperimentTable />).text()).toEqual(`Bar`)
   })
 
   test(`matches snapshot`, () => {
-    const tree = renderer.create(<MyComponent />).toJSON()
+    const tree = renderer.create(<ExperimentTable />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })

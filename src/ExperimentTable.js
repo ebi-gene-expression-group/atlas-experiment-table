99bbc52f8a38fcf2b02b7dbdce9efbe8a72a81ac
lingyun1010 <zhaolingyun1010@gmail.com>
2019-01-16 15:24:29 +0000

    Refactor table header and cell to make it generic

--- src/ExperimentTable.js
+++ src/ExperimentTable.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {TextInputField, SelectField, Table, Pane, minorScale } from 'evergreen-ui'
+import { Table } from 'evergreen-ui'
 import _ from 'lodash'
+import styled from 'styled-components'
 
-const headerToParam = headerText => {
-  switch (headerText) {
-  case 1:
-    return `lastUpdate`
-  case 2:
-    return `species`
-  case 3:
-    return `experimentDescription`
-  case 4:
-    return `experimentalFactors`
-  case 5:
-    return `numberOfAssays`
-  case 6:
-    return `downloads`
-  default:
-    return `lastUpdate`
-  }
-}
+const TabelCellDiv = styled.div`
+  font-size: 13px;
+  font-family: Helvetica, Arial, FreeSans, "Liberation Sans", sans-serif;
+`
 
 class ExperimentTable extends React.Component {
   constructor(props) {
@@ -33,17 +20,19 @@ class ExperimentTable extends React.Component {
       ordering: true,
       checkedArray: [],
       currentPage: 1,
-      userNumber: 1
+      userNumber: 10
     }
     this.sort = this.sort.bind(this)
-    this.renderTableHeaderCell = this.renderTableHeaderCell.bind(this)
+    this.renderTableSortHeaderCell = this.renderTableSortHeaderCell.bind(this)
     this.filter = this.filter.bind(this)
     this.renderTableSearchHeaderCell = this.renderTableSearchHeaderCell.bind(this)
     this.setValue = this.setValue.bind(this)
     this.handleCheckbox = this.handleCheckbox.bind(this)
+    this.renderTableBottomInfo = this.renderTableBottomInfo.bind(this)
+    this.renderTableHeader = this.renderTableHeader.bind(this)
   }
 
-  renderTableHeaderCell (columnNumber, headerText, width) {
+  renderTableSortHeaderCell (columnNumber, headerText, width) {
     return (
       <Table.TextHeaderCell
         flexBasis={width} flexShrink={0} flexGrow={0}
@@ -55,7 +44,7 @@ class ExperimentTable extends React.Component {
           `${headerText} `,
           columnNumber===this.state.orderedColumn ?
             this.state.ordering ?
-              <i className="icon icon-common icon-sort-up"/> : <i className="icon icon-common icon-sort-down"/>
+              <i className={`icon icon-common icon-sort-up`}/> : <i className={`icon icon-common icon-sort-down`}/>
             : <i className={`icon icon-common icon-sort`}/>
         ]}
       </Table.TextHeaderCell>
@@ -64,21 +53,19 @@ class ExperimentTable extends React.Component {
 
   sort(profiles) {
     const {ordering, orderedColumn} = this.state
-    let propKey = headerToParam(orderedColumn)
+    let propKey = this.props.tableHeader[orderedColumn].dataParam
     const filteredElements = _.sortBy(profiles, propKey)
     return ordering ? filteredElements :  filteredElements.reverse()
   }
 
   // Filter the profiles based on the name property.
-  filter(profiles){
+  filter(profiles, tableHeader){
     const searchQuery = this.state.searchQuery.trim()
-    // If the searchQuery is empty, return the profiles as is.
-    if (searchQuery.length === 0) return profiles
-
-    return profiles.filter(profile => _.isArray(profile[headerToParam(this.state.searchedColumn)]) ?
-      _.flattenDeep(profile[headerToParam(this.state.searchedColumn)]).toLowerCase().some(item=>item.includes(searchQuery.toLowerCase())) :
-      profile[headerToParam(this.state.searchedColumn)].toLowerCase().includes(searchQuery.toLowerCase())
-    )
+    return searchQuery.length === 0 ? profiles :
+      profiles.filter(profile => _.isArray(profile[tableHeader[this.state.searchedColumn].dataParam]) ?
+        _.flattenDeep(profile[tableHeader[this.state.searchedColumn].dataParam]).some(item=>item.toLowerCase().includes(searchQuery.toLowerCase())) :
+        profile[tableHeader[this.state.searchedColumn].dataParam].toLowerCase().includes(searchQuery.toLowerCase())
+      )
   }
 
   renderTableSearchHeaderCell(columnNumber, headerText, width){
@@ -111,68 +98,121 @@ class ExperimentTable extends React.Component {
     this.setState({checkedArray: checkedArray})
   }
 
+  renderTableBottomInfo(dataArray){
+    const {currentPage, userNumber} = this.state
+    const pageNumbersButton = []
+    for (let i = 1; i <= Math.ceil(dataArray.length/userNumber); i++) {
+      pageNumbersButton.push( i===currentPage ? <li className={`paginate_button current`} tabIndex={i}>{currentPage}</li> :
+        <li className={`paginate_button`} tabIndex={i}><a onClick={() => this.setState({currentPage: i})}>{i}</a></li>)
+    }
+    return [
+      <div className={`small-6 columns`}>
+        <div className={`dataTables_info`}>{dataArray.length === 0 ? `There is no search results under this query`
+          : `Found ${dataArray.length} entries filtered from ${this.props.aaData.length} total entries.`} </div>
+      </div>,
+      <div className={`small-6 columns`}>
+        <ul className={`pagination`}  style={{"textAlign": `right`}}>
+          {[
+            currentPage > 1 ?
+              <li  className={`paginate_button previous`}><a onClick={()=>{currentPage > 1 && this.setState({currentPage: currentPage-1})}}>Previous</a></li> :
+              <li  className={`paginate_button previous unavailable disabled`}>Previous</li>,
+            pageNumbersButton,
+            dataArray.length > currentPage*userNumber ?
+              <li className={ `paginate_button next`}><a onClick={()=>{dataArray.length >= currentPage*userNumber && this.setState({currentPage: currentPage+1})}}>Next</a></li> :
+              <li className={`paginate_button next unavailable disabled`}>Next</li>
+          ]}
+        </ul>
+      </div>
+    ]
+  }
+
+  renderTableHeader(tableHeader){
+    return tableHeader.map((header, index) => {
+      switch(header.type) {
+      case `plain`:
+        return <Table.TextHeaderCell flexBasis={header.width} flexShrink={0} flexGrow={0}>{header.title}</Table.TextHeaderCell>
+      case `sort`:
+        return this.renderTableSortHeaderCell(index, header.title, header.width)
+      case `search`:
+        return this.renderTableSearchHeaderCell(index, header.title, header.width)
+      default:
+        return <Table.TextHeaderCell flexBasis={header.width} flexShrink={0} flexGrow={0}>{header.title}</Table.TextHeaderCell>
+      }}
+    )
+  }
   render() {
     const {userSearch, userKingdom, checkedArray, userNumber, currentPage} = this.state
-    const dataArray = userSearch ? this.sort(this.props.aaData).filter(data => data && Object.values(data).some(value => value.toString().toLowerCase().includes(userSearch.toLowerCase()))) :
-      this.filter(this.sort(this.props.aaData)).filter(data=> userKingdom ? data.kingdom===userKingdom : true)
+    const {host, aaData, tableHeader, enableDownload} = this.props
+    const dataArray = userSearch ? this.sort(aaData).filter(data => data && Object.values(data).some(value => value.toString().toLowerCase().includes(userSearch.toLowerCase()))) :
+      this.filter(this.sort(aaData), tableHeader).filter(data=> userKingdom ? data.kingdom===userKingdom : true)
     const currentPageData = userNumber ? dataArray.slice(userNumber*(currentPage-1), userNumber*currentPage) : dataArray
 
     return [
-      <Pane display="flex">
-        <SelectField
-          id={`gxaExperimentsTableKingdomSelect`}
-          label="Kingdom"
-          marginRight={minorScale(4)}
-          onChange={event => this.setState({ userKingdom: event.target.value })}
-        >
-          <option value="" checked>All</option>
-          <option value="animals">Animals</option>
-          <option value="plants">Plants</option>
-          <option value="fungi">Fungi</option>
-        </SelectField>
-        <SelectField
-          label="Show entries"
-          marginRight={minorScale(4)}
-          onChange={this.setValue(`userNumber`)}
-        >
-          <option value="1" checked>1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="100">100</option>
-          <option value={this.props.aaData.length}>All</option>
-        </SelectField>
-        <TextInputField
-          label="Search all columns"
-          marginRight={minorScale(4)}
-          onChange={this.setValue(`userSearch`)}
-        />
+      <div className={`row expanded`}>
+        <div className={`large-2 medium-4 small-8 columns`}>
+          <label> Kingdom: <input type={`hidden`} className={`search_init`}/>
+            <select defaultValue={`All`}
+              onChange={event => this.setState({userKingdom: event.target.value})}>
+              <option value="" selected>All</option>
+              <option value="plants">Plants</option>
+              <option value="animals">Animals</option>
+              <option value="fungi">Fungi</option>
+            </select>
+          </label>
+        </div>
+        <div className={`large-2 medium-4 small-8 columns`}>
+          <label>Entries per page:
+            <select defaultValue={10} onChange={this.setValue(`userNumber`)}>
+              <option value="10">10</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+              <option value={aaData.length}>All</option>
+            </select>
+          </label>
+        </div>
+        <div className={`small-8 columns`}>
+          <div className={`dataTables_filter`}><label>Search all columns:
+            <input type={`search`} placeholder={`Type here ...`}
+              onChange={this.setValue(`userSearch`)}/></label>
+          </div>
+        </div>
+      </div>,
 
-      </Pane>,
       <Table>
         <Table.Head>
           <Table.TextHeaderCell flexBasis={60} flexShrink={0} flexGrow={0}>Index</Table.TextHeaderCell>
-          {this.renderTableHeaderCell(1, `Loaded date`, 120)}
-          {this.renderTableSearchHeaderCell(2, `species`, 160)}
-          {this.renderTableSearchHeaderCell(3, `experiment description`, 300)}
-          {this.renderTableSearchHeaderCell(4, `experimental variables`, 260)}
-          {this.renderTableHeaderCell(5, `Number of assays`, 160)}
-          <Table.TextHeaderCell><a onClick={()=>alert(checkedArray)}>Download {checkedArray.length} entries</a></Table.TextHeaderCell>
+          {[
+            this.renderTableHeader(tableHeader),
+            enableDownload && <Table.TextHeaderCell><a onClick={()=>alert(checkedArray)}>Download {checkedArray.length} entries</a></Table.TextHeaderCell>
+          ]}
         </Table.Head>
         <Table.Body>
           {currentPageData.map((data, index) => {
             return (
-              <Table.Row height="auto" backgroundColor={index%2===0?`white`:`rgb(237, 240, 242)`} paddingY={14} key={index}>
-                <Table.TextCell flexBasis={60} flexShrink={0} flexGrow={0}>{`${index + 1 + userNumber*(currentPage-1)} `}</Table.TextCell>
-                <Table.TextCell flexBasis={160} flexShrink={0} flexGrow={0}>{data.lastUpdate}</Table.TextCell>
-                <Table.TextCell flexBasis={160} flexShrink={0} flexGrow={0}>{data.species}</Table.TextCell>
-                <Table.Cell flexBasis={360} flexShrink={0} flexGrow={0}>{data.experimentDescription}</Table.Cell>
-                <Table.TextCell flexBasis={260} flexShrink={0} flexGrow={0}>
-                  <ul>{data.experimentalFactors.map(factor => <li key={index+factor}>{factor}</li>)}</ul>
-                </Table.TextCell>
-                <Table.TextCell flexBasis={160} flexShrink={0} flexGrow={0}>{data.numberOfAssays}</Table.TextCell>
-                <Table.TextCell flexBasis={60} flexShrink={0} flexGrow={0}>
-                  <input type="checkbox" checked={checkedArray.includes(data.experimentAccession)} onChange={()=>this.handleCheckbox(data.experimentAccession)} />
-                </Table.TextCell>
+              <Table.Row height="auto" backgroundColor={index%2===0 ? `white`:`#F5F6F7`} paddingY={14} key={index}>
+
+                <Table.Cell flexBasis={60} flexShrink={0} flexGrow={0}>
+                  <TabelCellDiv>{`${index + 1 + userNumber*(currentPage-1)} `}</TabelCellDiv>
+                </Table.Cell>
+                {[
+                  tableHeader.map((header) => {
+                    const cellItem = _.isArray(data[header.dataParam]) ?
+                      <ul>{data[header.dataParam].map(factor => <li key={index+factor}>{factor}</li>)}</ul> : data[header.dataParam]
+                    return <Table.Cell flexBasis={header.width} flexShrink={0} flexGrow={0}>
+                      <TabelCellDiv>
+                        {header.link ? <a href={`${host}/${header.resource}/${data[header.link]}/${header.endpoint}`}>{cellItem}</a> : cellItem}
+                      </TabelCellDiv>
+                    </Table.Cell>
+                  }
+                  ),
+                  enableDownload && <Table.Cell flexBasis={60} flexShrink={0} flexGrow={0}>
+                    <TabelCellDiv>
+                      <input type={`checkbox`} checked={checkedArray.includes(data.experimentAccession)}
+                        onChange={()=>this.handleCheckbox(data.experimentAccession)} />
+                    </TabelCellDiv>
+                  </Table.Cell>
+                ]}
               </Table.Row>)
           })
           }
@@ -180,22 +220,7 @@ class ExperimentTable extends React.Component {
       </Table>,
       <br />,
       <div className={`row`}>
-        <div className={`small-6 columns`}>
-          <div className={`dataTables_info`}>{dataArray.length === 0 ? `There is no search results under this query`
-            : `Found ${dataArray.length} entries filtered from ${this.props.aaData.length} total entries.`} </div>
-        </div>
-        <div className={`small-6 columns`}><div className={`dataTables_paginate paging_simple_numbers`}>
-          <ul className={`pagination`}>
-            <li className={currentPage>1? `paginate_button previous` : `paginate_button previous unavailable disabled`} aria-controls="experiments-table" tabIndex="0"
-              id="experiments-table_previous"  onClick={()=>{currentPage>1&&this.setState({currentPage: currentPage-1})}} >Previous
-            </li>
-            <li className="paginate_button current" aria-controls="experiments-table" tabIndex="0">{currentPage}</li>
-            <li className={dataArray.length>=currentPage*userNumber ? `paginate_button next`:`paginate_button next unavailable disabled`} aria-controls="experiments-table" tabIndex="0"
-              id="experiments-table_next" onClick={()=>{dataArray.length>=currentPage*userNumber && this.setState({currentPage: currentPage+1})}}>Next
-            </li>
-          </ul>
-        </div>
-        </div>
+        {this.renderTableBottomInfo(dataArray)}
       </div>
 
     ]
@@ -203,7 +228,11 @@ class ExperimentTable extends React.Component {
 }
 
 ExperimentTable.propTypes = {
-  aaData: PropTypes.array
+  aaData: PropTypes.array,
+  host: PropTypes.string.isRequired,
+  resource: PropTypes.string.isRequired,
+  tableHeader: PropTypes.array.isRequired,
+  enableDownload: PropTypes.bool.isRequired
 }
 
 export default ExperimentTable

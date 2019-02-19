b8e4785fd3b3d9354139da918f9f9cf99e469afa
lingyun1010 <zhaolingyun1010@gmail.com>
2019-02-19 09:37:32 +0000

    Move the styled.div into table component intead of
    passing by props

--- html/render.js
+++ html/render.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import styled from 'styled-components'
 
 import ExperimentTable from '../src/index.js'
 
-const TableCellDiv = styled.div`
-  font-size: 13px;
-  font-family: Helvetica, Arial, FreeSans, "Liberation Sans", sans-serif;
-`
-
 const render = (options, target) => {
-  ReactDOM.render(<ExperimentTable {...options} TableCellDiv={TableCellDiv}/>, document.getElementById(target))
+  ReactDOM.render(<ExperimentTable {...options} />, document.getElementById(target))
 }
 
 export {render}

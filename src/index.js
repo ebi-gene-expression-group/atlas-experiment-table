9e30c4d12d132fc4a0292e4ca7d75408a779c588
lingyun1010 <zhaolingyun1010@gmail.com>
2019-03-21 13:15:14 +0000

    Unwrap fetch loader and add an example  response

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import withFetchLoader from 'atlas-react-fetch-loader'
 import ExperimentTable from './ExperimentTable'
 
-export default withFetchLoader(ExperimentTable)
+export default ExperimentTable

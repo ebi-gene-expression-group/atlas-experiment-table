312b4832d3789ee790a5fd08ae314907557bc501
lingyun1010 <zhaolingyun1010@gmail.com>
2019-03-29 15:55:12 +0000

    Rmove junk html tag and className, refactor code

--- src/TableFooter.js
+++ src/TableFooter.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TableFooter = ({dataArray, currentPage, selectedNumber, onChange, data}) => {
-  const pageNumbersButton = []
-  for (let i = 1; i <= Math.ceil(dataArray.length/selectedNumber); i++) {
-    pageNumbersButton.push( i === currentPage ? <li className={`paginate_button number current`} key={`bottom${i}`}>{currentPage}</li> :
-      <li className={`paginate_button number`} key={`bottom${i}`}><a onClick={() => onChange(i)}>{i}</a></li>)
+const TableFooter = ({dataArrayLength, currentPage, entryPerPage, onChange, dataLength}) => {
+  const pageNumbers = []
+  for (let i = 1; i <= Math.ceil(dataArrayLength / entryPerPage); i++) {
+    pageNumbers.push( i === currentPage ? <li className={`current`} key={`bottom${i}`}>{currentPage}</li> :
+      <li key={`bottom${i}`}><a onClick={() => onChange(i)}>{i}</a></li>)
   }
   return [
-    <div key={`bottom-info`} className={`small-6 columns`}>
-      <div className={`dataTables_info`}>{dataArray.length === 0 ? `There is no search results under this query`
-        : `Showing ${dataArray.length} out of ${data.length} experiments.`} </div>
+    <div key={`bottom-info`} className={`small-6 columns padding-top-medium`}>
+      {
+        dataArrayLength === 0 ?
+          `There are no results for this query.` :
+          `Showing ${dataArrayLength} out of ${dataLength} experiments.`
+      }
     </div>,
-    <div key={`bottom-button`} className={`small-6 columns`}>
-      <ul className={`pagination`}  style={{"textAlign": `right`}}>
+    <div key={`bottom-button`} className={`small-6 columns padding-top-medium`}>
+      <ul className={`pagination`} style={{textAlign: `right`}}>
         {[
           currentPage > 1 ?
-            <li key={`previous`} className={`paginate_button previous`}><a className={`previous`} onClick={()=>{currentPage > 1 && onChange(currentPage - 1)}}>Previous</a></li> :
-            <li key={`previous`} className={`paginate_button previous unavailable disabled`}>Previous</li>,
-          pageNumbersButton,
-          dataArray.length > currentPage*selectedNumber ?
-            <li key={`next`} className={ `paginate_button next`}><a className={`next`} onClick={()=>{dataArray.length >= currentPage*selectedNumber && onChange(currentPage + 1)}}>Next</a></li> :
-            <li key={`next`} className={`paginate_button next unavailable disabled`}>Next</li>
+            <li key={`previous`}><a onClick={() => {currentPage > 1 && onChange(currentPage - 1)}}>Previous</a></li> :
+            <li key={`previous`} className={`disabled`}>Previous</li>,
+          pageNumbers,
+          dataArrayLength > currentPage * entryPerPage ?
+            <li key={`next`}><a onClick={() => {dataArrayLength >= currentPage * entryPerPage && onChange(currentPage + 1)}}>Next</a></li> :
+            <li key={`next`} className={`disabled`}>Next</li>
         ]}
       </ul>
     </div>
@@ -29,10 +32,10 @@ const TableFooter = ({dataArray, currentPage, selectedNumber, onChange, data}) =
 }
 
 TableFooter.propTypes = {
-  dataArray: PropTypes.array.isRequired,
+  dataArrayLength: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
-  selectedNumber: PropTypes.number.isRequired,
-  data: PropTypes.array.isRequired,
+  entryPerPage: PropTypes.number.isRequired,
+  dataLength: PropTypes.number.isRequired,
   onChange: PropTypes.func.isRequired
 }
 

fe3cd3402ac38d8548ed1441994b9c8d6f8de1e4
lingyun1010 <zhaolingyun1010@gmail.com>
2019-03-29 16:49:17 +0000

    Amend test after clean html classnames

--- __test__/TableFooter.test.js
+++ __test__/TableFooter.test.js
@@ -2,25 +2,25 @@ import React from 'react'
 import Enzyme from 'enzyme'
 import {shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { data} from './TestUtils'
+import { data } from './TestUtils'
 import TableFooter from '../src/TableFooter'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe(`TableFooter`, () => {
   const props = {
-    dataArray: data,
-    currentPage: 1,
+    dataArrayLength: Math.random() * 10,
+    entryPerPage: 1,
     selectedNumber: 2,
-    data: data,
-    onChange: ()=>{}
+    dataLength:  data.length,
+    onChange: () => {}
   }
 
   test(`should render a previous button, a next button and information text`, () => {
     const wrapper = shallow(<TableFooter {...props}/>)
-    expect(wrapper.find('li.next')).toHaveLength(1)
-    expect(wrapper.find('li.previous')).toHaveLength(1)
-    expect(wrapper.find(`.dataTables_info`)).toHaveLength(1)
+    expect(wrapper.find(`li`).first().key()).toBe(`previous`)
+    expect(wrapper.find(`li`).last().key()).toBe(`next`)
+    expect(wrapper.find(`div`).first().key()).toBe(`bottom-info`)
   })
 
 })
